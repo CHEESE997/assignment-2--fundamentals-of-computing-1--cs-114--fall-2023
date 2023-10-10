3653657f9efdf40f3e94f37b82dbc41147519277
CHEESE997 <143614071+CHEESE997@users.noreply.github.com>
2023-10-10 00:44:04 -0400

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -3,25 +3,42 @@ import java.util.Scanner;
 
 public class Diamond {
   public static void main(String[] args) {
-    //open scanner
+    //Initialize times
+    int times =0;
     Scanner scan = new Scanner(System.in);
-    //Input
+    //Input number
     System.out.println("Please enter a number:");
     int size = scan.nextInt();
     //check if input is odd or even using mod
-    //if input is even
+    //if input is even using mod
     if((size%2)==0) {
       System.out.println("Your diamond is even");
       int half = (size/2);
+
     }
     //if input is odd
     else{
       System.out.println("Your diamond is odd");
-      int half = ((size+1)/2);
-      //does half the diamond
-        for(int times = 0; times < half; times++){
+      //rounds the diamond up in size
+      var halfup = ((size+1)/2);
+      //does half the diamond, increasing
+        while(times < halfup){
+          //draws spaces draws, draws half the input, then decreases by 1 each time
+         for(int space1=((halfup-times)) ; 0 < space1;space1--){
+            System.out.print(" ");
+          }
+          //draws astarisks, guarenteed to draw once, then 2 more times each time the amount of times goes up by 1
+          for(int astarisk = 1+(times*2); 0 < astarisk; astarisk--){
+            System.out.print("*");
+          }
+          System.out.println();
+          times++;
+        }
+        times = (halfup-2);
+        //other half of the diamond, decreasing
+        while(times >= 0){
           //draws spaces
-         for(int space1=((half-times)) ; 0 < space1;space1--){
+          for(int space1=((halfup-times)) ; 0 < space1;space1--){
             System.out.print(" ");
           }
           //draws astarisks
@@ -29,11 +46,9 @@ public class Diamond {
             System.out.print("*");
           }
           System.out.println();
-
-       }
-    }
-
-    //close scan
-    scan.close();
+          times--;
+        }
+      }
+      scan.close();
   }
 }
